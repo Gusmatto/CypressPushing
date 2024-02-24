d4fdf6f3ce21edd15662ee7d99e3a212d0fbc004
Gusmatto <gustavo.matto46@gmail.com>
2024-02-24 12:14:39 -0300

    api login

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '59gs5f',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -10,4 +11,11 @@ module.exports = defineConfig({
     defaultCommandTimeout: 10000,
     fixturesFolder: 'cypress/e2e/',
   },
+
+  env: {
+    user: 'pushingit',
+    pass: '123456!',
+    baseUrlApi: 'https://pushing-it.onrender.com/api',
+    token: ''
+  }
 });
